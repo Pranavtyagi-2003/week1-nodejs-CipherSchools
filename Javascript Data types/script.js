fe2f5cf28e29cadb4f53f7c9dcbe8d263e5351c0
Pranav <pranav.12014487@lpu.in>
2023-06-22 19:26:24 +0530

    JavaScript Datatypes

---
+++ Javascript Data types/script.js
@@ -0,0 +1,38 @@
+//Strings
+let number = "raining";
+let string = "String as a name";
+
+let String = 20083838
+console.log(number.toUpperCase())
+
+console.log("Hello and good evening".length);
+console.log("hello".charAt(4));
+
+
+//Boolean (true : 1 , false : 0)
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof isNaN("five"));
+
+//Truthy values : any value except 0 and false
+//Falsy values : 0 false '' NaN undefined null
+
+//Null Undefined
+let score;
+let result = null
+
+if(score){
+    console.log("Very true !!");
+}
+else{
+    console.log("Very wrong !!");
+}
+
+console.log('hello'.includes('e'));
+
+
+
+
+console.log(typeof score);
+console.log(typeof result);
